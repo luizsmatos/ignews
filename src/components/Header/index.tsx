dac1b7786378e652b756fd6541f73b6493d37b9d
luizsmatos <gusstavopdm@gmail.com>
2021-11-16 09:49:52 -0300

    feature: Componente SignInButton e Estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
-import styles from './styles.module.scss'
+import Image from 'next/image';
+import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image 
+        src="/images/logo.svg" 
+        alt="ig.news"
+        width={110}
+        height={31}
+         />
         <nav>
-          <a href="">Home</a>
+          <a href="" className={styles.active}>Home</a>
           <a href="">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
-  )
+  );
 }
