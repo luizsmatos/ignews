eff9155d17e57e74141c99a5f5b4752d9603f16e
luizsmatos <gusstavopdm@gmail.com>
2021-11-16 08:00:09 -0300

    build: Sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World em NextJS!!</h1>
+      <h1 className={styles.title}>Hello World em NextJS!!</h1>
     </div>
   )
 }
