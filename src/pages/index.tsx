6432c6e44d20426344557cfa3a3fe4430a69904d
luizsmatos <gusstavopdm@gmail.com>
2021-11-16 08:29:57 -0300

    style: Estilização Global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Hello World em NextJS!!</h1>
-    </div>
-  )
+    <>
+      <Head>
+        <title> Início | ig.news</title>
+      </Head>
+      <h1>Hello World em NextJS!!</h1>
+    </>
+  );
 }
